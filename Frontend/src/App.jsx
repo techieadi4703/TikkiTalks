dd0f5a6951d7881f5c139141ccb8c00a64839473
techieadi4703 <techie.adi47@gmail.com>
2025-01-21 22:19:10 +0530

    created login and signup pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./components/HomePage";
-import SignupPage from "./components/SignupPage";
-import LoginPage from "./components/LoginPage";
-import SettingsPage from "./components/SettingsPage";
-import ProfilePage from "./components/ProfilePage";
+import Navbar from "./pages/Navbar";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { checkAuth, authUser, isCheckingAuth } = useAuthStore();
@@ -28,11 +29,14 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={authUser?<HomePage/>:<Navigate to={"/login"}/>} />
-        <Route path="/signup" element={authUser?<SignupPage />:<Navigate to={"/"}/>} />
-        <Route path="/login" element={authUser?<LoginPage />:<Navigate to={"/"}/>} />
+        {/* <Route path="/signup" element={authUser?<SignupPage />:<Navigate to={"/"}/>} /> */}
+        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path="/login" element={authUser?<LoginPage />:<Navigate to={"/"}/>} /> */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser?<ProfilePage />:<Navigate to={"/login"}/>} />
       </Routes>
+      <Toaster />
       {/* <h1 className="text-2xl font-extrabold text-red-600">Hello Babby</h1>
       <div className="text-3xl">hello</div> */}
     </>
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -19,4 +20,32 @@ export const useAuthStore = create((set) => ({
       set({ isCheckingAuth: false });
     }
   },
+
+  signup: async(data)=>{
+    set({isSigningUp: true});
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({authUser: res.data});
+      toast.success("Account created successfully");
+      console.log("res.data.message");
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.error("(FE) Error in signup route:\n", error);
+    }finally{
+      set({isSigningUp: false});
+    }
+  },
+
+  logout: async()=>{
+    try {
+      const res=await axiosInstance.post("/auth/logout");
+      set({authUser: null});
+      toast.success("Logged out successfully");
+      console.log("res.data.message");
+    } catch (error){
+      toast.error(error.response.data.message);
+      console.error("(FE) Error in logout route:\n", error);
+    }
+  }
+
 }));
